5a5eae9ae5a9b9101ed37871c10f16e9861718db
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-04 01:47:12 +0200

    redux init

---
+++ src/navigation/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+function Navigation() {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  );
+}
+
+export default Navigation;
