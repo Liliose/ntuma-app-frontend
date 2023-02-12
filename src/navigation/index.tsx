16cca03272c8852db9542107dfd7a7e06b3718b8
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-12 13:59:16 +0200

    removed un used stack animation config

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -35,26 +35,6 @@ const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 const TopTab = createMaterialTopTabNavigator();
 
-const config = {
-  animation: 'spring',
-  config: {
-    stiffness: 1000,
-    damping: 50,
-    mass: 3,
-    overshootClamping: false,
-    restDisplacementThreshold: 0.01,
-    restSpeedThreshold: 0.01,
-  },
-};
-
-const closeConfig = {
-  animation: 'timing',
-  config: {
-    duration: 500,
-    easing: Easing.linear,
-  },
-};
-
 function Navigation() {
   const loadData = useLoadBasiData();
   useEffect(() => {
