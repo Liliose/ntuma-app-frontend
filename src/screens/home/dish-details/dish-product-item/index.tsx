7d1aa4020e198cb3b859f2941eca2958ad85f577
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-03-04 06:36:08 +0200

    adding dish products to cart

--- src/screens/home/dish-details/dish-product-item/index.tsx
+++ src/screens/home/dish-details/dish-product-item/index.tsx
@@ -1,7 +1,8 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Pressable} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import WhiteCard from '../../../../components/white-card';
 import {
+  ICartItem,
   IDishProduct,
   INavigationProp,
   IProduct,
@@ -12,12 +13,29 @@ import {viewFlexSpace} from '../../../../constants/styles';
 import {app} from '../../../../constants/app';
 import {APP_COLORS} from '../../../../constants/colors';
 import {currencyFormatter} from '../../../../helpers';
+import Icon from 'react-native-vector-icons/Entypo';
 interface IDishProductItemProps extends INavigationProp {
   dishProduct: IDishProduct;
+  handleProductPriceChange: any;
 }
-const DishProductItem = ({navigation, dishProduct}: IDishProductItemProps) => {
+
+const initialPrice: ICartItem = {
+  price: 0,
+  ppId: 0,
+  productId: 0,
+  priceType: '' as any,
+  customPrice: false,
+  quantity: 1,
+};
+const DishProductItem = ({
+  navigation,
+  dishProduct,
+  handleProductPriceChange,
+}: IDishProductItemProps) => {
   const {products} = useSelector((state: RootState) => state.products);
+  const {prices} = useSelector((state: RootState) => state.productPrices);
   const [product, setProduct] = useState<IProduct | undefined>(undefined);
+  const [price, setPrice] = useState<ICartItem>(initialPrice);
   useEffect(() => {
     let r: boolean = true;
     if (r) {
@@ -31,6 +49,48 @@ const DishProductItem = ({navigation, dishProduct}: IDishProductItemProps) => {
     };
   }, [dishProduct]);
 
+  useEffect(() => {
+    if (product?.priceType === 'single') {
+      setPrice({
+        ...initialPrice,
+        productId: product?.pId as number,
+        priceType: product?.priceType as string,
+        price: product?.priceType === 'single' ? product.singlePrice : 0,
+      });
+    } else {
+      const allPrices = prices.filter(item => item.productId === product?.pId);
+      if (allPrices.length > 0) {
+        //get small price
+        const smallPrice = allPrices.reduce((min, current) => {
+          return current.amount < min.amount ? current : min;
+        });
+        //get small price
+        setPrice({
+          ...initialPrice,
+          productId: product?.pId as number,
+          priceType: product?.priceType as string,
+          price: smallPrice.amount,
+          ppId: smallPrice.ppId,
+        });
+      }
+    }
+  }, [product]);
+
+  const handlePlus = () => {
+    setPrice({...price, quantity: price.quantity + 1});
+  };
+  const handleMinus = () => {
+    if (price.quantity - 1 >= 0) {
+      setPrice({...price, quantity: price.quantity - 1});
+    } else {
+      setPrice({...price, quantity: 0});
+    }
+  };
+
+  useEffect(() => {
+    handleProductPriceChange(price);
+  }, [price]);
+
   return (
     <View style={{marginVertical: 10}}>
       <WhiteCard style={{padding: 10}}>
@@ -43,8 +103,38 @@ const DishProductItem = ({navigation, dishProduct}: IDishProductItemProps) => {
           <View style={{flex: 1, marginHorizontal: 10}}>
             <Text style={{color: APP_COLORS.BLACK}}>{product?.name}</Text>
             <Text style={{color: APP_COLORS.TEXT_GRAY}}>
-              {currencyFormatter(product?.singlePrice)} Rwf
+              {currencyFormatter(price.price * price.quantity)} Rwf
             </Text>
+          </View>
+          <View style={[viewFlexSpace]}>
+            <Pressable onPress={() => handleMinus()}>
+              <View
+                style={{
+                  backgroundColor: APP_COLORS.MAROON,
+                  borderRadius: 100,
+                  padding: 3,
+                }}>
+                <Icon name="minus" size={25} color={APP_COLORS.WHITE} />
+              </View>
+            </Pressable>
+            <Text
+              style={{
+                marginHorizontal: 10,
+                color: APP_COLORS.TEXT_GRAY,
+                fontSize: 20,
+              }}>
+              {price.quantity}
+            </Text>
+            <Pressable onPress={() => handlePlus()}>
+              <View
+                style={{
+                  backgroundColor: APP_COLORS.MAROON,
+                  borderRadius: 100,
+                  padding: 3,
+                }}>
+                <Icon name="plus" size={25} color={APP_COLORS.WHITE} />
+              </View>
+            </Pressable>
           </View>
         </View>
       </WhiteCard>
--- src/screens/home/dish-details/index.tsx
+++ src/screens/home/dish-details/index.tsx
@@ -1,13 +1,132 @@
-import {View, Text, Image, ScrollView} from 'react-native';
-import React from 'react';
-import {IDish, INavigationPropWithRouteRequired} from '../../../../interfaces';
+import {View, Text, Image, ScrollView, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  ICartItem,
+  IDish,
+  INavigationPropWithRouteRequired,
+} from '../../../../interfaces';
 import {APP_COLORS} from '../../../constants/colors';
-import {viewFlexCenter, viewFlexSpace} from '../../../constants/styles';
+import {
+  btnWithBgContainerStyles,
+  btnWithBgTextStyles,
+  viewFlexCenter,
+  viewFlexSpace,
+} from '../../../constants/styles';
 import {app} from '../../../constants/app';
 import DishProductItem from './dish-product-item';
+import {currencyFormatter} from '../../../helpers';
+import {ALERT_TYPE, Dialog} from 'react-native-alert-notification';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../../../reducers';
+import {addCartItem} from '../../../actions/cart';
 
+const initialPrice: ICartItem = {
+  price: 0,
+  ppId: 0,
+  productId: 0,
+  priceType: '' as any,
+  customPrice: false,
+  quantity: 1,
+};
 const DishDetails = ({route, navigation}: INavigationPropWithRouteRequired) => {
+  const dispatch = useDispatch();
   const {dish} = route.params as {dish: IDish};
+  const {products} = useSelector((state: RootState) => state.products);
+  const {prices} = useSelector((state: RootState) => state.productPrices);
+  const [productPrices, setProductPrices] = useState<ICartItem[]>([]);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const handleProductPriceChange = (cartItem: ICartItem) => {
+    const index = productPrices.findIndex(
+      item => item.productId === cartItem.productId,
+    );
+    if (index > -1) {
+      const newState = [...productPrices];
+      newState[index] = cartItem;
+      setProductPrices(newState);
+    } else {
+      setProductPrices([...productPrices, cartItem]);
+    }
+  };
+
+  useEffect(() => {
+    setTotalPrice(prevPrice => {
+      let sum = 0;
+      for (let i = 0; i < productPrices.length; i++) {
+        sum +=
+          Number(productPrices[i].price) * Number(productPrices[i].quantity);
+      }
+      return sum;
+    });
+  }, [productPrices]);
+
+  //initial product prices
+  useEffect(() => {
+    const newState: ICartItem[] = [];
+    for (let i = 0; i < dish.products.length; i++) {
+      const product = products.find(
+        item => item.pId === dish.products[i].productId,
+      );
+      if (product) {
+        if (product?.priceType === 'single') {
+          newState.push({
+            ...initialPrice,
+            productId: product?.pId as number,
+            priceType: product?.priceType as string,
+            price: product?.priceType === 'single' ? product.singlePrice : 0,
+          });
+        } else {
+          const allPrices = prices.filter(
+            item => item.productId === product?.pId,
+          );
+          if (allPrices.length > 0) {
+            //get small price
+            const smallPrice = allPrices.reduce((min, current) => {
+              return current.amount < min.amount ? current : min;
+            });
+            //get small price
+            newState.push({
+              ...initialPrice,
+              productId: product?.pId as number,
+              priceType: product?.priceType as string,
+              price: smallPrice.amount,
+              ppId: smallPrice.ppId,
+            });
+          }
+        }
+      }
+    }
+    setProductPrices(newState);
+  }, []);
+  //initial product prices
+
+  const handleAddToCart = () => {
+    if (totalPrice === 0) {
+      Dialog.show({
+        type: ALERT_TYPE.DANGER,
+        title: 'Warning',
+        textBody:
+          "All Product's Price on this dish can not be zero. Please increase the quantity of at leat one product.",
+        button: 'OK',
+        // onHide: () => {},
+      });
+    } else {
+      for (let i = 0; i < productPrices.length; i++) {
+        if (productPrices[i].quantity > 0) {
+          dispatch(addCartItem(productPrices[i]));
+        }
+      }
+      Dialog.show({
+        type: ALERT_TYPE.SUCCESS,
+        title: 'Success',
+        textBody: 'Cart updated successful!',
+        button: 'OK',
+        onHide: () => {
+          navigation.navigate('Cart');
+        },
+      });
+    }
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: APP_COLORS.MAROON}}>
       <View
@@ -26,7 +145,7 @@ const DishDetails = ({route, navigation}: INavigationPropWithRouteRequired) => {
           />
         </View>
 
-        <View style={{padding: 20}}>
+        <View style={{padding: 20, flex: 1}}>
           <View style={[viewFlexSpace]}>
             <View>
               <Text
@@ -37,7 +156,9 @@ const DishDetails = ({route, navigation}: INavigationPropWithRouteRequired) => {
                 }}>
                 {dish.name}
               </Text>
-              <Text style={{color: APP_COLORS.TEXT_GRAY}}>Ingredients</Text>
+              <Text style={{color: APP_COLORS.TEXT_GRAY}}>
+                {dish.products.length} Ingredients
+              </Text>
             </View>
             {dish.utubeLink.trim() !== '' && (
               <View style={[viewFlexCenter]}>
@@ -52,15 +173,26 @@ const DishDetails = ({route, navigation}: INavigationPropWithRouteRequired) => {
               </View>
             )}
           </View>
-          <ScrollView showsVerticalScrollIndicator={false}>
+          <ScrollView>
             {dish.products.map((item, index) => (
               <DishProductItem
                 key={index}
                 navigation={navigation}
                 dishProduct={item}
+                handleProductPriceChange={handleProductPriceChange}
               />
             ))}
           </ScrollView>
+          <View style={{padding: 10}}>
+            <Text style={{color: APP_COLORS.BLACK}}>
+              Total: {currencyFormatter(totalPrice)} Rwf
+            </Text>
+          </View>
+          <Pressable onPress={() => handleAddToCart()}>
+            <View style={[btnWithBgContainerStyles]}>
+              <Text style={[btnWithBgTextStyles]}>Add to cart</Text>
+            </View>
+          </Pressable>
         </View>
       </View>
     </View>
