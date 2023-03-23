b142773a5b3b2c118a038a9e19f3863df68886c0
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-03-23 20:52:12 +0200

    firebase cloud messaging config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {LogBox} from 'react-native';
 import {AlertNotificationRoot} from 'react-native-alert-notification';
+import messaging from '@react-native-firebase/messaging';
 import Navigation from './src/navigation';
 import {store} from './src/store';
 import {subscribeToSocket, unSubscribeToSocket} from './src/worker/socket';
@@ -9,9 +10,21 @@ LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
 
+const requestCloudMessagingNotificationPermissionFromUser = async () => {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+};
+
 function App(): JSX.Element {
   useEffect(() => {
     subscribeToSocket(store);
+    requestCloudMessagingNotificationPermissionFromUser();
 
     // window.addEventListener("online", handleOnline);
     // window.addEventListener("offline", handleOffline);
